c0ee2eca72ea1e3e599968f6fe7f5c3211961956
kumavis <aaron@kumavis.me>
2023-07-01 16:09:19 -1000

    fix message submit

--- src/new-message-form.js
+++ src/new-message-form.js
@@ -1,20 +1,30 @@
 import React from "react";
 
+// supported in at least chrome for android
+// https://caniuse.com/mdn-api_navigator_useragentdata
+const isMobile = navigator.userAgentData.mobile;
+
 export default class NewMessageForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
   }
 
-  handleChange(event) {
-      this.setState({value: event.target.value});
+  handleChange (event) {
+    this.setState({value: event.target.value});
   }
-  handleSubmit(event) {
+  handleSubmit (event) {
     event.preventDefault();
     this.props.onSubmit(this.state.value);
     this.setState({value: ''});
+  }
+  onKeyDown (event) {
+    if (event.key === 'Enter') {
+      this.handleSubmit(event);
+    }
   }
 
   render() {
@@ -25,8 +35,13 @@ export default class NewMessageForm extends React.Component {
           placeholder='your message...'
           value={this.state.value}
           onChange={this.handleChange}
-          enterKeyHint="go"
+          onKeyDown={this.onKeyDown}
         />
+        {isMobile && (
+          <button type='submit' className='new-message-button'>
+            <span role='img' aria-label='send'>📨</span>
+          </button>
+        )}
       </form>
     );
   }
